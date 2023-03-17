106e3a47ca318fe8c815be76d822dbd31cac6b86
john_song <32257362+yhsrzbg@users.noreply.github.com>
2023-03-17 14:36:10 +0800

    Update example-custom-script-1.sh

--- example-custom-script-1.sh
+++ example-custom-script-1.sh
@@ -36,3 +36,17 @@
 #EOF
 #git apply example.patch
 
+echo "src-git small8 https://github.com/kenzok8/small-package" >> ./feeds.conf.default
+
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+pushd luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
