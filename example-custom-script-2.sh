9ac2bfdd7be32d0df04ec9805123badf58745f44
john_song <32257362+yhsrzbg@users.noreply.github.com>
2023-03-21 11:22:13 +0800

    Update example-custom-script-2.sh

--- example-custom-script-2.sh
+++ example-custom-script-2.sh
@@ -16,4 +16,4 @@
 #patch content
 #EOF
 #git apply example.patch
-sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
